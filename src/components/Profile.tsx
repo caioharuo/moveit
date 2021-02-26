db2acf99b484918262da33917eb6f8b79da76ac8
caioharuo <caio.haruo@hotmail.com>
2021-02-26 19:07:46 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext)
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/caioharuo.png" alt="Caio Haruo" />
+      <div>
+        <strong>Caio Haruo</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
